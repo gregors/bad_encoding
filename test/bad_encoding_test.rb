62326b8c939f248e1a7bead2cfdaed4be11c4cbb
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2020-07-18 00:15:03 -0400

    formatting

--- test/bad_encoding_test.rb
+++ test/bad_encoding_test.rb
@@ -7,11 +7,11 @@ class BadEncodingTest < Minitest::Test
 
   def test_it_finds_bad_encoding
     bad_string = "r. \xED\xA0\xBD\xED\xB9\x82 )</p>\n</div>\n        </div>\n\n"
-    assert(bad_string.show_bad_encoding  == bad_string)
+    assert(bad_string.show_bad_encoding == bad_string)
   end
 
   def test_show_msg_when_no_bad_encoding
     good_string = "hey you guys!"
-    assert(good_string.show_bad_encoding  == "no bad segments found")
+    assert(good_string.show_bad_encoding == "no bad segments found")
   end
 end
